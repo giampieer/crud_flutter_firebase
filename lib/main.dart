ea565f68730c485f8f92e09bfbb8cebd933ef1e1
giampieer <giampieer24@gmail.com>
2018-11-11 18:00:06 -0500

    init project

---
+++ lib/main.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/material.dart';
+import 'package:crud_flutter_firebase/ui/listview_note.dart';
+
+void main() => runApp(
+  MaterialApp(
+    title: 'Returning Data',
+    home: ListViewNote(),
+  ),
+);
